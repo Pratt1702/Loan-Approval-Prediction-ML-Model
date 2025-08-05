7d928ae9560c679e62e12356f3fea8099ac341b9
Pratheeeeek <teampratgaming1@gmail.com>
2025-08-05 22:22:06 +0530

    cors

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173,https://loan-prediction-frontend.vercel.app"],  # Adjust the port if necessary
+    allow_origins=["http://localhost:5173","https://loan-prediction-frontend.vercel.app"],  # Adjust the port if necessary
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
